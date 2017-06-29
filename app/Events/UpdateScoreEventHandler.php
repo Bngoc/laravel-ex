6501e00fd40aa56d881d8f701f6134398e669e57
DC-VIETIS\ngoc.buiquang <ngoc.buiquang@vietis.com.vn>
2017-06-29 17:52:23 +0700

    fo

---
+++ app/Events/UpdateScoreEventHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use App\Events\Event;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class UpdateScoreEventHandler extends Event
+{
+    use SerializesModels;
+
+    CONST EVENT = 'score.update';
+    CONST CHANNEL = 'score.update';
+
+    public function handle($data)
+    {
+        $redis = Redis::connection();
+        $redis->publish(self::CHANNEL, $data);
+    }
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+}
+//http://www.volkomenjuist.nl/blog/2013/10/20/laravel-4-and-nodejsredis-pubsub-realtime-notifications/
