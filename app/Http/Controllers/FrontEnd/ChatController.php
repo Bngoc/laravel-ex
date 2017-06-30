c5abd866e0e142b887a4cdd86fd06aba44fd0bb0
DC-VIETIS\ngoc.buiquang <ngoc.buiquang@vietis.com.vn>
2017-06-30 17:42:13 +0700

    dsd

--- app/Http/Controllers/FrontEnd/ChatController.php
+++ app/Http/Controllers/FrontEnd/ChatController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\FrontEnd;
 
 use Illuminate\Http\Request;
-use Redis;
+use LRedis;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class ChatController extends Controller
 {
     public function sendMessage(){
-        $redis = Redis::connection();
+        $redis = LRedis::connection();
         $data = ['message' => Request::input('message'), 'user' => Request::input('user')];
         $redis->publish('message', json_encode($data));
         return response()->json([]);
